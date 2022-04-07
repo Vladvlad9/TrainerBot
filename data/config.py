355c9314d3d558dd1036ec23feb3f4ebb274b922
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-07 20:43:51 +0300

    new project

---
+++ data/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv(".env")
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+DATABASE = os.environ["DATABASE"]
+
+
+async def load_admins() -> tuple:
+    return tuple(map(int, environ["ADMINS"].split(",")))
---
+++ utils/notify_admins.py
@@ -0,0 +1,13 @@
+import logging
+from aiogram import Dispatcher
+
+from data.config import load_admins
+
+
+async def on_startup_notify(dp: Dispatcher):
+    for admin in await load_admins():
+        try:
+            await dp.bot.send_message(admin, "Бот Запущен и готов к работе")
+
+        except Exception as err:
+            logging.exception(err)
