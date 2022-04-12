3d652c53b0dd6725fcb719322a33f387eb6a5fa3
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-12 16:40:45 +0300

    add new usersin bd

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,8 +2,10 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
+from utils.db_api.crud import UsersCrud
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f'Привет, {message.from_user.full_name}!')
+    UsersCrud.add_user(id_users=1, name='ivan', position='users')
