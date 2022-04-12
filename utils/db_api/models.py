57370a7b084e609bf8609e925a9027ae09d2a3e1
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-12 16:40:22 +0300

    Create new models: -Staff_Rates -Motivational_Programs -Career -Standards

--- utils/db_api/models.py
+++ utils/db_api/models.py
@@ -8,6 +8,33 @@ class Users(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    id_users = Column(Integer, primary_key=True)
-    name = Column(Text, primary_key=True)
+    id_users = Column(Integer, nullable=False)
+    name = Column(Text, nullable=False)
+    positions = Column(Text, nullable=False)
 
+
+class Staff_Rates(Base):
+    __tablename__ = 'staff_rates'
+
+    id = Column(Integer, primary_key=True)
+    description = Column(Text, nullable=True)
+
+
+class Motivational_Programs(Base):
+    __tablename__ = 'motivational_programs'
+    id = Column(Integer, primary_key=True)
+    description = Column(Text, nullable=True)
+
+
+class Career(Base):
+    __tablename__ = 'career'
+    id = Column(Integer, primary_key=True)
+    description = Column(Text, nullable=True)
+    img = Column(Text)
+
+
+class Standards(Base):
+    __tablename__ = 'standards'
+    id = Column(Integer, primary_key=True)
+    description = Column(Text, nullable=True)
+    img = Column(Text)
